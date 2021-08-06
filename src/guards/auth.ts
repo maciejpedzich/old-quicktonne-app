f5f54cae698c84dc67ad54a748da7682e0f40a52
Maciej Pędzich <dnalop.me@gmail.com>
2021-08-06 12:23:57 +0200

    Create and use auth route guard for About page

---
+++ src/guards/auth.ts
@@ -0,0 +1,35 @@
+import { NavigationGuardNext, RouteLocationNormalized } from 'vue-router';
+
+import useAuth0 from '@/composables/useAuth0';
+import { watchEffect } from '@vue/runtime-dom';
+
+export default function authGuard(
+  from: RouteLocationNormalized,
+  to: RouteLocationNormalized,
+  next: NavigationGuardNext
+): Promise<void> | void {
+  const { isLoading, isAuthenticated } = useAuth0();
+
+  const checkAuthStatus = async () => {
+    if (isAuthenticated.value) {
+      return next();
+    }
+
+    /*
+      Vue router throws up a warning if the next function is never called.
+      In order to solve this problem, we redirect the user to LogIn route.
+      That's where we call Auth0 SDK's loginWithRedirect method
+    */
+    return next({ name: 'LogIn' });
+  };
+
+  if (!isLoading.value) {
+    return checkAuthStatus();
+  }
+
+  watchEffect(() => {
+    if (!isLoading.value) {
+      return checkAuthStatus();
+    }
+  });
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
+
 import Home from '../views/Home.vue';
+import authGuard from '@/guards/auth';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,11 +12,13 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue'),
+    beforeEnter: authGuard
+  },
+  {
+    path: '/login',
+    name: 'LogIn',
+    component: () => import('../views/LogIn.vue')
   }
 ];
 
