342dd75ed7a0663ffebba697221b1661112ce972
Maciej Pędzich <dnalop.me@gmail.com>
2021-08-06 12:23:23 +0200

    Create log in page

---
+++ src/views/LogIn.vue
@@ -0,0 +1,28 @@
+<template>
+  <p>
+    You should now be automatically redirected to Auth0 page. But if you can
+    still see this message, click the button below to log in.
+  </p>
+  <Button label="Log in" @click="loginWithRedirect" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Button from 'primevue/button';
+
+import useAuth0 from '@/composables/useAuth0';
+
+export default defineComponent({
+  name: 'LogIn',
+  components: {
+    Button
+  },
+  setup() {
+    const { loginWithRedirect } = useAuth0();
+
+    loginWithRedirect();
+
+    return { loginWithRedirect };
+  }
+});
+</script>
