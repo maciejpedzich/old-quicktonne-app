728e508f9af4c8a66bad839f18bc989b8355e00b
Maciej Pędzich <dnalop.me@gmail.com>
2021-08-06 12:24:24 +0200

    Create navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,25 @@
 <template>
-  <Menubar :model="menuItems">
-    <template #start>
-      <strong class="mx-2 md:mx-5">Quicktonne</strong>
-    </template>
-  </Menubar>
-  <div class="container mx-3 md:mx-5">
+  <Navbar />
+  <div class="container mt-7 md:mt-8">
     <router-view />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import Menubar from 'primevue/menubar';
+import { defineComponent } from 'vue';
+import Navbar from '@/components/Navbar.vue';
+
+import useAuth0 from '@/composables/useAuth0';
 
 export default defineComponent({
   name: 'App',
   components: {
-    Menubar
+    Navbar
   },
   setup() {
-    const menuItems = ref([
-      {
-        label: 'Home',
-        icon: 'pi pi-home',
-        to: '/'
-      },
-      {
-        label: 'About',
-        icon: 'pi pi-info-circle',
-        to: '/about'
-      },
-      {
-        label: 'Source code',
-        icon: 'pi pi-github',
-        url: 'https://github.com/maciejpedzich/quicktonne-app',
-        target: '_blank'
-      }
-    ]);
+    const { handleRedirectCallback } = useAuth0();
 
-    return { menuItems };
+    handleRedirectCallback();
   }
 });
 </script>
@@ -48,5 +29,37 @@ body {
   color: var(--text-color);
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
     Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  background-color: var(--surface-0);
+  color: var(--text-color);
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+.container {
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+  margin-bottom: 8rem;
+}
+@media (min-width: 768px) {
+  .container {
+    width: 750px;
+  }
+}
+@media (min-width: 992px) {
+  .container {
+    width: 970px;
+  }
+}
+@media (min-width: 1200px) {
+  .container {
+    width: 1170px;
+  }
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,58 @@
+<template>
+  <Menubar class="top-0 w-full fixed" :model="menuItems">
+    <template #start>
+      <strong class="mx-2 md:mx-5">Quicktonne</strong>
+    </template>
+  </Menubar>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue';
+import Menubar from 'primevue/menubar';
+
+import useAuth0 from '@/composables/useAuth0';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Menubar
+  },
+  setup() {
+    const { isAuthenticated, logout } = useAuth0();
+
+    const menuItems = computed(() => [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+        to: '/'
+      },
+      {
+        label: 'About',
+        icon: 'pi pi-info-circle',
+        to: '/about'
+      },
+      {
+        label: 'Source code',
+        icon: 'pi pi-github',
+        url: 'https://github.com/maciejpedzich/quicktonne-app',
+        target: '_blank',
+        class: 'md:mr-2 md:pr-2 border-right-1 border-100'
+      },
+      {
+        label: 'Log in',
+        icon: 'pi pi-sign-in',
+        to: '/login',
+        visible: !isAuthenticated.value
+      },
+      {
+        label: 'Log out',
+        icon: 'pi pi-sign-out',
+        command: logout,
+        visible: isAuthenticated.value
+      }
+    ]);
+
+    return { menuItems };
+  }
+});
+</script>
