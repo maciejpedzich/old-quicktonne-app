d6923a9a98d658594943689479cdf6721bcb10a6
Maciej Pędzich <dnalop.me@gmail.com>
2021-08-13 19:41:18 +0200

    Use PrimeVue's toast service in the Vue app

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 
 import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
 import 'primevue/resources/primevue.min.css';
 import 'primevue/resources/themes/arya-blue/theme.css';
 import 'primeflex/primeflex.css';
@@ -9,4 +10,6 @@ import 'primeicons/primeicons.css';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(PrimeVue).use(router).mount('#app');
+const app = createApp(App);
+
+app.use(PrimeVue).use(ToastService).use(router).mount('#app');
