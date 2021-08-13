e16a225bd472adc61cec66ba28eebc5eeee1bbc9
Maciej Pędzich <dnalop.me@gmail.com>
2021-08-13 19:51:11 +0200

    Move HeroContainer markup to Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,48 @@
 <template>
-  <p v-if="isAuthenticated">Show lobby here, because we're logged in</p>
-  <HomeHeroContainer v-else />
+  <div class="align-self-center text-center">
+    <h1 class="text-6xl m-0">Welcome to Quicktonne!</h1>
+    <div v-if="isAuthenticated" class="mt-5">
+      <router-link tag="button" to="/rooms/create">
+        <Button class="p-button-lg" icon="pi pi-plus" label="Create a room" />
+      </router-link>
+      <Divider align="center">
+        <span>OR</span>
+      </Divider>
+      <!-- <router-link tag="button" to="/rooms"> -->
+      <Button
+        class="p-button-lg p-button-outlined"
+        icon="pi pi-search"
+        label="Browse rooms"
+      />
+      <!-- </router-link> -->
+    </div>
+    <div v-else>
+      <p class="text-2xl mt-3 mb-4">
+        Catchy slogan goes here, think about it later...
+      </p>
+      <Button class="p-button-lg" label="Join now" @click="loginWithRedirect" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-import HomeHeroContainer from '@/components/HomeHeroContainer.vue';
+import Button from 'primevue/button';
+import Divider from 'primevue/divider';
+
 import useAuth0 from '@/composables/useAuth0';
 
 export default defineComponent({
   name: 'Home',
   components: {
-    HomeHeroContainer
+    Button,
+    Divider
   },
   setup() {
-    const { isAuthenticated } = useAuth0();
+    const { isAuthenticated, loginWithRedirect } = useAuth0();
 
-    return { isAuthenticated };
+    return { isAuthenticated, loginWithRedirect };
   }
 });
 </script>
