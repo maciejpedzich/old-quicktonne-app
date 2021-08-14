fe09b91c77be0a08a7567b148c9476413697d01b
Maciej Pędzich <dnalop.me@gmail.com>
2021-08-14 15:27:11 +0200

    Move useBrowseRooms to rooms folder

--- src/composables/rooms/useBrowseRooms.ts
+++ src/composables/rooms/useBrowseRooms.ts
@@ -12,6 +12,7 @@ import {
 import db from '@/db';
 import Room from '@/types/models/Room';
 import DocWithId from '@/types/models/DocWithId';
+import LANGUAGES from '@/constants/LANGUAGES';
 
 import logErrorInDevMode from '@/utils/logErrorInDevMode';
 import createFirestoreConverter from '@/utils/createFirestoreConverter';
@@ -26,25 +27,6 @@ export default function useBrowseRooms() {
 
   const isLoading = ref(true);
   const rooms = ref<DocWithId<Room>[]>([]);
-
-  const allLanguages = ref<{ label: string; value: string }[]>([
-    { label: 'C (GCC 9.2.0)', value: 'c' },
-    { label: 'C++ (GCC 9.2.0)', value: 'cpp' },
-    { label: 'C# (Mono 6.6.0.161)', value: 'csharp' },
-    { label: 'Clojure (1.10.1)', value: 'clojure' },
-    { label: 'Go (1.13.5)', value: 'go' },
-    { label: 'Groovy (3.0.3)', value: 'groovy' },
-    { label: 'Java (OpenJDK 13.0.1)', value: 'java' },
-    { label: 'JavaScript (Node.js 12.14.0)', value: 'javascript' },
-    { label: 'Lua (5.3.5)', value: 'lua' },
-    { label: 'Objective-C (Clang 7.0.1)', value: 'objective-c' },
-    { label: 'Perl (5.28.1)', value: 'perl' },
-    { label: 'Python (3.8.1)', value: 'python' },
-    { label: 'Ruby (2.7.0)', value: 'ruby' },
-    { label: 'Rust (1.40.0)', value: 'rust' },
-    { label: 'Swift (5.2.3)', value: 'swift' },
-    { label: 'TypeScript (3.7.4)', value: 'typescript' }
-  ]);
   const preferredLanguages = ref<string[]>(['javascript']);
 
   let cancelRoomUpdatesSub: (() => void) | null = null;
@@ -88,7 +70,7 @@ export default function useBrowseRooms() {
   return {
     isLoading,
     rooms,
-    allLanguages,
+    LANGUAGES,
     preferredLanguages,
     getAndSubToRoomUpdates,
     cancelRoomUpdatesSub
