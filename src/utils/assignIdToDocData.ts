e048854b76002a32e5947cc9e015ab553992beef
Maciej Pędzich <dnalop.me@gmail.com>
2021-08-15 20:46:39 +0200

    Add DocumentSnapshot as possible doc type

--- src/utils/assignIdToDocData.ts
+++ src/utils/assignIdToDocData.ts
@@ -1,8 +1,8 @@
-import { QueryDocumentSnapshot } from 'firebase/firestore';
+import { DocumentSnapshot, QueryDocumentSnapshot } from 'firebase/firestore';
 import DocWithId from '@/types/models/DocWithId';
 
 export default function assignIdToDocData<T>(
-  doc: QueryDocumentSnapshot<T>
+  doc: QueryDocumentSnapshot<T> | DocumentSnapshot<T>
 ): DocWithId<T> {
   return Object.assign({ id: doc.id }, doc.data());
 }
